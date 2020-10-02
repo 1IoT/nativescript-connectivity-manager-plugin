ed9b041474cdb6df1eb2f74e14d1336a9dc2f499
Upd4ting <upd4ting@gmail.com>
2020-10-02 14:32:47 +0200

    add ios wifi connect and get ssid

--- src/connectivity-manager-impl.ios.ts
+++ src/connectivity-manager-impl.ios.ts
@@ -1,73 +1,107 @@
-import {Common} from './connectivity-manager-impl.common';
-import {ConnectivityManagerInterface} from "./connectivity-manager-interface";
+import { Common } from "./connectivity-manager-impl.common";
+import { ConnectivityManagerInterface } from "./connectivity-manager-interface";
 
 /**
-* It manages the connectivity API of an iOS mobile device.
-* This is especially thought for applications where an app needs to connect to a Wi-Fi AP for P2P communication.
-* It allows also to switch back to a network with internet connection to also to internet requests.
- *
- * TODO if you want to implement this for iOS please make a pull-request =)
-*/
-export class ConnectivityManagerImpl extends Common implements ConnectivityManagerInterface {
+ * It manages the connectivity API of an iOS mobile device.
+ * This is especially thought for applications where an app needs to connect to a Wi-Fi AP for P2P communication.
+ * It allows also to switch back to a network with internet connection to also to internet requests.
+ */
+export class ConnectivityManagerImpl
+  extends Common
+  implements ConnectivityManagerInterface {
+  public getSSID(): string {
+    let interfaceNames = <NSArray<string>>CNCopySupportedInterfaces();
 
-    public getSSID(): string {
-        // Not implemented yet
-        return undefined;
+    for (let i = 0; i < interfaceNames.count; i++) {
+      let info = CNCopyCurrentNetworkInfo(interfaceNames[i]);
+      if (!info) {
+        continue;
+      }
+      let ssid = info[kCNNetworkInfoKeySSID];
+      if (!ssid) {
+        continue;
+      }
+      return ssid;
     }
 
-    public getWifiNetworkId(): number {
-        // Not implemented yet
-        return undefined;
-    }
+    return null;
+  }
 
-    public isWifiEnabled(): boolean {
-        // Not implemented yet
-        return undefined;
-    }
+  public getWifiNetworkId(): number {
+    // Not implemented yet
+    return undefined;
+  }
 
-    public isWifiConnected(): boolean {
-        // Not implemented yet
-        return undefined;
-    }
+  public isWifiEnabled(): boolean {
+    // Not implemented yet
+    return undefined;
+  }
 
-    public isCellularEnabled(): boolean {
-        // Not implemented yet
-        return undefined;
-    }
+  public isWifiConnected(): boolean {
+    // Not implemented yet
+    return undefined;
+  }
 
-    public isCellularConnected(): boolean {
-        // Not implemented yet
-        return undefined;
-    }
+  public isCellularEnabled(): boolean {
+    // Not implemented yet
+    return undefined;
+  }
 
-    public isGpsEnabled(): boolean {
-        // Not implemented yet
-        return undefined;
-    }
+  public isCellularConnected(): boolean {
+    // Not implemented yet
+    return undefined;
+  }
 
-    public isGpsConnected(): boolean {
-        // Not implemented yet
-        return undefined;
-    }
+  public isGpsEnabled(): boolean {
+    // Not implemented yet
+    return undefined;
+  }
 
-    public hasInternet(): boolean {
-        //Not implemented yet
-        return undefined
-    }
+  public isGpsConnected(): boolean {
+    // Not implemented yet
+    return undefined;
+  }
 
+  public hasInternet(): boolean {
+    //Not implemented yet
+    return undefined;
+  }
 
-    public scanWifiNetworks(): Promise<string[]> {
-        // Not implemented yet
-        return undefined;
-    }
+  public scanWifiNetworks(): Promise<string[]> {
+    // Not implemented yet
+    return undefined;
+  }
 
-    public async connectToWifiNetwork(ssid: string, password: string, milliseconds: number): Promise<boolean> {
-        // Not implemented yet
-        return undefined;
-    }
+  public connectToWifiNetwork(
+    ssid: string,
+    password: string,
+    milliseconds: number
+  ): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      let configuration = NEHotspotConfiguration.new().initWithSSIDPassphraseIsWEP(
+        ssid,
+        password,
+        false
+      );
+      configuration.joinOnce = true;
 
-    public disconnectWifiNetwork(timeoutMs: number): Promise<boolean>  {
-        // Not implemented yet
-        return undefined;
-    }
+      NEHotspotConfigurationManager.sharedManager.applyConfigurationCompletionHandler(
+        configuration,
+        (err) => {
+          if (err && err instanceof NSError) {
+            resolve(false);
+          } else if (this.getSSID() == ssid) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      );
+    });
+  }
+
+  public disconnectWifiNetwork(timeoutMs: number): Promise<boolean> {
+    // Not implemented yet
+    return undefined;
+  }
 }
