d19cc898989904a7cbef0790f919b57751666d57
Upd4ting <upd4ting@gmail.com>
2020-10-06 11:24:05 +0200

    Update reference file

--- src/references.d.ts
+++ src/references.d.ts
@@ -2,4 +2,5 @@
  * Sets the API level for the native platforms.
  */
 
-/// <reference path="./node_modules/@nativescript/types/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android-26.d.ts" />
